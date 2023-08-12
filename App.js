1b40aa05afc98a05949238aead99311a27f5fdf5
akhipath03 <akhipath03@gmail.com>
2023-08-12 00:54:28 -0400

    Fixed HomeScreen Routing

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import CameraScreen from "./components/CameraScreen";
 import TextDisplayScreen from "./components/DisplayScreen.js";
+import HomeScreen from "./components/HomeScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Camera">
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Camera" component={CameraScreen} />
         <Stack.Screen name="TextDisplay" component={TextDisplayScreen} />
       </Stack.Navigator>
